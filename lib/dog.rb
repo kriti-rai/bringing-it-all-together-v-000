9fbe471b629061553fdc8e6bd23eff541a5008d0
Kriti Rai <kriti.rai@gmail.com>
2018-04-28 05:05:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -108,9 +108,9 @@ class Dog
 
     row = DB[:conn].execute(sql, dog.name, dog.breed)[0]
     # binding.pry
-    if !row.empty?
-      new_dog = self.new_from_db(row)
-      self.find_by_id(new_dog.id)
+    if !!row
+      self.new_from_db(row)
+      # self.find_by_id(new_dog.id)
     else
       self.create(hash)
     end
